a600f1fbc607a363b4bb94b931e07ff544ee7ceb
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-14 17:07:34 -0300

    Finalizando GetAllVideosController

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateCategoryController } from "./controllers/CreateCategoryController
 import { CreateVideoController } from "./controllers/CreateVideoController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
+import { GetAllVideosController } from "./controllers/GetAllVideosController";
 import { GetCategoriesByIdController } from "./controllers/GetCategoriesByIdController";
 import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 
@@ -15,4 +16,5 @@ routes.delete("/categories/:id", new DeleteCategoryController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
 
 routes.post("/videos", new CreateVideoController().handle);
+routes.get("/videos", new GetAllVideosController().handle);
 export { routes };
