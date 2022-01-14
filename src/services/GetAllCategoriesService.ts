d9c5474f843d8841ad9e6d7e2c04ac1fea4d73c7
Diego Araújo <diegoedaraujo@gmail.com>
2022-01-14 16:26:18 -0300

    Order category by name

--- src/services/GetAllCategoriesService.ts
+++ src/services/GetAllCategoriesService.ts
@@ -1,10 +1,15 @@
-import { getRepository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import { Category } from "../entities/Category";
 
 export class GetAllCategoriesService {
   async execute(): Promise<Category[] | Error> {
-    const repo = getRepository(Category);
-    const categories = await repo.find();
+    // const repo = getRepository(Category);
+    // const categories = await repo.find();
+    // return categories;
+    const categories = await getRepository(Category)
+      .createQueryBuilder("category")
+      .orderBy("name")
+      .getMany();
     return categories;
   }
 }
